b10fe6178fc6f207f038f6dc77866f0cec93fc18
adrianorosa02-seducsp <adrianorosa02@prof.educacao.sp.gov.br>
2026-04-24 15:33:11 -0300

    Update config_thinkpad_python.ps1

--- seduc/config_thinkpad_python.ps1
+++ seduc/config_thinkpad_python.ps1
@@ -12,6 +12,13 @@ $pythonUrl = "https://www.python.org/ftp/python/$pythonVersion/python-$pythonVer
 $installDir = "$env:USERPROFILE\Python$pythonVersion"
 #$installDir = "C:\Program Files\Python313" 
 
+#Instalação GH
+
+
+
+## 4. Tenta salvar permanentemente no nível de USUÁRIO (Não precisa de Admin)
+#[Environment]::SetEnvironmentVariable("Path", $CURRENT_PATH, "User")
+
 # Criar pasta de instalação
 if (-Not (Test-Path $installDir)) {
     New-Item -ItemType Directory -Path $installDir | Out-Null
@@ -28,6 +35,20 @@ Expand-Archive -Path $zipPath -DestinationPath $installDir -Force
 
 # Remover arquivo ZIP temporário
 Remove-Item $zipPath
+
+# Variavel onde estará o GH (Deve ser ajustada conforme infraestrutura)
+#$GH_PATH     = "C:\Users\Docker\AppData\Local\github\" # Onde você descompactou o gh.exe
+
+# Copia o arquivo gh_2.88.1_windows_amd64.zip do git para posterior descompactação
+# .zip é obrigatorio pelas politicas de segurança do github/wget
+wget -O $env:USERPROFILE+"/ghzip.zip"+ "https://github.com/adrianorosa02-seducsp/versionamento/raw/refs/heads/main/gh_2.88.1_windows_amd64.zip"
+# 1. Define o caminho do arquivo
+$zipPath = "$env:USERPROFILE\ghzip.zip"
+$destPath = "$env:USERPROFILE\gh\"
+Invoke-WebRequest -Uri "https://github.com/adrianorosa02-seducsp/versionamento/raw/refs/heads/main/gh_2.88.1_windows_amd64.zip" -OutFile "$env:USERPROFILE\ghzip.zip"
+
+Expand-Archive -Path $zipPath -DestinationPath $destPath -Force
+Remove-Item -Path $zipPath -Force
 
 # Adicionar Python ao PATH do usuário (sem admin)
 $envPath = [Environment]::GetEnvironmentVariable("Path", "User")
