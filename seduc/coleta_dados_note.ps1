b27ad2fad9e42a8a723afd9ca851d53a5ccde53b
adrianorosa02-seducsp <adrianorosa02@prof.educacao.sp.gov.br>
2026-07-24 12:09:50 -0300

    Update coleta_dados_note.ps1

--- seduc/coleta_dados_note.ps1
+++ seduc/coleta_dados_note.ps1
@@ -1,3 +1,12 @@
+# Envia os dados da auditoria para um webhook (ex: Power Automate, Google Sheets ou servidor próprio)
+$dados = @{
+    Equipamento   = $nomeMaquina
+    Auditor       = $auditorAtual
+    UltimoUsuario = $ultimoUsuarioAnterior.Usuario
+    DataHoraLogin = $ultimoUsuarioAnterior.DataHora
+} | ConvertTo-Json
+
+Invoke-RestMethod -Uri "https://n8n.inetz.com.br/webhook-test/api_auditoria" -Method Post -Body $dados -ContentType "application/json"
 try {
      Invoke-RestMethod -Uri $uriWebhook -Method Post -Body $jsonBody -ContentType "application/json; charset=utf-8"
      Write-Host "`n[+] Dados de auditoria enviados com sucesso para o n8n!" -ForegroundColor Green
